fee7b7e135a263a7a83ba95bf19919a25b346adc
Dinda Ayu Syafitri <85775024+dinda-ayu-syafitri@users.noreply.github.com>
2025-01-17 21:49:42 +0700

    Initial Commit

---
+++ AlephTechnicalTest/AlephTechnicalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlephTechnicalTestApp.swift
+//  AlephTechnicalTest
+//
+//  Created by Dinda Ayu Syafitri on 17/01/25.
+//
+
+import SwiftUI
+
+@main
+struct AlephTechnicalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
