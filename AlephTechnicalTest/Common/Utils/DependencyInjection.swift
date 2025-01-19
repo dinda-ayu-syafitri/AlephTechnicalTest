33e6328d4f5778ec2c750b9d713317e75fcc3cf4
Dinda Ayu Syafitri <85775024+dinda-ayu-syafitri@users.noreply.github.com>
2025-01-19 11:56:48 +0700

    Add: set up dependency injection

---
+++ AlephTechnicalTest/Common/Utils/DependencyInjection.swift
@@ -0,0 +1,17 @@
+//
+//  DependencyInjection.swift
+//  AlephTechnicalTest
+//
+//  Created by Dinda Ayu Syafitri on 19/01/25.
+//
+
+import Foundation
+
+class DependencyInjection: ObservableObject {
+    static let shared = DependencyInjection()
+
+    lazy var categoriesRemoteDataSource = CategoriesRemoteDataSource()
+    lazy var categoriesRepository = CategoriesRepository(categoryRemoteDataSource: categoriesRemoteDataSource)
+
+    lazy var getAllCategoriesUseCase = GetAllCategoriesUseCaseImpl(categoriesRepository: categoriesRepository)
+}
