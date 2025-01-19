b4487974e846d179b6d38e369004a3aa4829674f
Dinda Ayu Syafitri <85775024+dinda-ayu-syafitri@users.noreply.github.com>
2025-01-19 19:45:57 +0700

    Add: navigation to item detail from category items

---
+++ AlephTechnicalTest/Presentation/ItemDetail/ItemDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemDetailView.swift
+//  AlephTechnicalTest
+//
+//  Created by Dinda Ayu Syafitri on 19/01/25.
+//
+
+import SwiftUI
+
+struct ItemDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemDetailView()
+}
