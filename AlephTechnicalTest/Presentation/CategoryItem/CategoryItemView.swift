6667ee618b7b7faaf1e1ff9ce25672280a016e59
Dinda Ayu Syafitri <85775024+dinda-ayu-syafitri@users.noreply.github.com>
2025-01-19 14:33:02 +0700

    Add: category item view

--- AlephTechnicalTest/Presentation/CategoryItem/CategoryItemView.swift
+++ AlephTechnicalTest/Presentation/CategoryItem/CategoryItemView.swift
@@ -8,10 +8,37 @@
 import SwiftUI
 
 struct CategoryItemView: View {
+    @Environment(\.dismiss) var dismiss
     @Binding var categoryId: Int?
 
     var body: some View {
-        Text("This is Category \(categoryId ?? 0) Item View")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], alignment: .leading, spacing: 20) {
+                    ItemThumbnailView()
+                    ItemThumbnailView()
+                    ItemThumbnailView()
+                }
+                .padding(.horizontal, 20)
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: {
+                    dismiss()
+                }) {
+                    Image(systemName: "chevron.left")
+                        .fontWeight(.semibold)
+                }
+            }
+
+            ToolbarItem(placement: .principal) {
+                Button(action: {}) {
+                    Text("Category \(categoryId ?? 0)")
+                }
+            }
+        }
+        .navigationBarBackButtonHidden()
     }
 }
 
---
+++ AlephTechnicalTest/Presentation/CategoryItem/ItemThumbnailView.swift
@@ -0,0 +1,48 @@
+//
+//  ItemThumbnailView.swift
+//  AlephTechnicalTest
+//
+//  Created by Dinda Ayu Syafitri on 19/01/25.
+//
+
+import SwiftUI
+
+struct ItemThumbnailView: View {
+    var body: some View {
+        VStack {
+            AsyncImage(url: URL(string: "https://fastly.picsum.photos/id/717/200/300.jpg?hmac=OJYQhMLNcYlN5qz8IR345SJ7t6A0vyHzT_RdMxO4dSc")) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
+
+            } placeholder: {
+                Color.red
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 150)
+            .clipShape(.rect(cornerRadius: 5))
+            .padding(5)
+
+            VStack(alignment: .leading) {
+                Text("Item 1")
+                    .font(.system(size: 15, weight: .bold))
+                    .multilineTextAlignment(.leading)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 5)
+                Text("This is the description")
+                    .font(.system(size: 12))
+                    .multilineTextAlignment(.leading)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .padding(.horizontal, 20)
+            .padding(.bottom, 20)
+        }
+        .background(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.1), radius: 5)
+    }
+}
+
+#Preview {
+    ItemThumbnailView()
+}
